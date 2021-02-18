ee3652657f80e920d8589048f4364ce31550d04f
Elis <elisalvarez@msn.com>
2021-02-18 19:10:36 +0100

    App Finalizada

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,4 @@
-import 'package:contador/src/pages/home_page.dart';
+import 'package:contador/src/pages/contador_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -6,8 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home:
-      Center(child: HomePage() ),
+      Center(
+          child: ContadorPage() ),
     );
   }
 }
